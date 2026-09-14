096f3455ca13a77631408048e798517bb3740071
sruizvargas11-dotcom <sebastian.ruiz.vargas@est.una.ac.cr>
2026-09-13 18:06:04 -0600

    backup antes de reestructura

--- sistema-reservas/src/main/java/una/eif206/data/XMLHelper.java
+++ sistema-reservas/src/main/java/una/eif206/data/XMLHelper.java
@@ -12,42 +12,42 @@ import java.nio.file.StandardCopyOption;
 
 public class XMLHelper {
 
-    private String path;
-    private static XMLHelper theInstance;
+  private String path;
+  private static XMLHelper theInstance;
 
-    public static XMLHelper instance() {
-        if (theInstance == null) theInstance = new XMLHelper("data.xml");
-        return theInstance;
-    }
+  public static XMLHelper instance() {
+    if (theInstance == null) theInstance = new XMLHelper("data.xml");
+    return theInstance;
+  }
 
-    public XMLHelper(String p) {
-        path = p;
-    }
+  public XMLHelper(String p) {
+    path = p;
+  }
 
-    public Data load() throws Exception {
-        JAXBContext jaxbContext = JAXBContext.newInstance(Data.class);
-        try (FileInputStream is = new FileInputStream(path)) {
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            return (Data) unmarshaller.unmarshal(is);
-        }
+  public Data load() throws Exception {
+    JAXBContext jaxbContext = JAXBContext.newInstance(Data.class);
+    try (FileInputStream is = new FileInputStream(path)) {
+      Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+      return (Data) unmarshaller.unmarshal(is);
     }
+  }
 
-    public void store(Data d) throws Exception {
-        File destino = new File(path);
-        File temporal = new File(path + ".tmp");
-        try {
-            JAXBContext jaxbContext = JAXBContext.newInstance(Data.class);
-            try (FileOutputStream os = new FileOutputStream(temporal)) {
-                Marshaller marshaller = jaxbContext.createMarshaller();
-                marshaller.marshal(d, os);
-                os.flush();
-            }
-            Files.move(temporal.toPath(), destino.toPath(), StandardCopyOption.REPLACE_EXISTING);
-        } catch (Exception e) {
-            if (temporal.exists()) {
-                temporal.delete();
-            }
-            throw e;
-        }
+  public void store(Data d) throws Exception {
+    File destino = new File(path);
+    File temporal = new File(path + ".tmp");
+    try {
+      JAXBContext jaxbContext = JAXBContext.newInstance(Data.class);
+      try (FileOutputStream os = new FileOutputStream(temporal)) {
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.marshal(d, os);
+        os.flush();
+      }
+      Files.move(temporal.toPath(), destino.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    } catch (Exception e) {
+      if (temporal.exists()) {
+        temporal.delete();
+      }
+      throw e;
     }
+  }
 }
